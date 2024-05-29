12166c87a0f417599fea24b5994375b9dd7fe43d
lance-e <437653103@qq.com>
2024-05-29 17:09:38 +0800

    feat:print char

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,42 @@
+#include "print.h"
 int main(void){
+	put_char('L');
+	put_char('O');
+	put_char('O');
+	put_char('N');
+	put_char('G');
+	put_char('-');
+	put_char('O');
+	put_char('S');
+	put_char('\n');
+	put_char('l');
+	put_char('l');
+	put_char('\b');
+	put_char('a');
+	put_char('n');
+	put_char('c');
+	put_char('e');
+	put_char('\n');
+	put_char('!');
+	put_char('\n');
+	put_char('6');
+	put_char('\n');
+	put_char('6');
+	put_char('\n');
+	put_char('6');
+	put_char('\n');
+	put_char('6');
+	put_char('\n');
+	put_char('6');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+	put_char('\n');
+
 	while(1);
 	return 0;
 }
