868896b91164ceff1ddb4a93d227cfc3f24fcc06
lance-e <437653103@qq.com>
2024-07-09 19:51:43 +0800

    feat:malloc and free syscall

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -46,6 +46,12 @@ uint32_t write(char* str){
 	return _syscall1(SYS_WRITE,str);
 }
 
+void* malloc(uint32_t size){
+	return _syscall1(SYS_MALLOC,size);
+}
 
+void free(void* ptr){
+	return _syscall1(SYS_FREE,ptr);
+}
 
 	
